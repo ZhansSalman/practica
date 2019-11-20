17b5543ee5af7d96079cf3d20da0370ea4c6cb27
ZhansSalman <57898099+ZhansSalman@users.noreply.github.com>
2019-11-20 15:54:21 +0600

    Delete 11.cpp

--- 11.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <cmath>
-using namespace std;
-int main() {
-	float PI = 3.14159265;
-	float x = -2.34;
-	float a = (abs(x - 5) - sin(x * PI / 180)) / 3 + sqrt(x * x + 2014) * cos(2 * x * PI / 180) - 3;
-	cout << a;
-}
